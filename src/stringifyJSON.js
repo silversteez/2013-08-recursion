44d92be8152c9ca804efb78518b8230f29b0f03b
Hack Reactor Students <github@hackreactor.com>
2013-08-05 15:39:46 -0700

    add working array, number, and string function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,22 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var result = "";
+  if (typeof obj === "string") {
+    result +='"' + obj + '"';
+  } else if (typeof obj === "number"){
+    result += obj.toString();
+  }
+  else if(Array.isArray(obj)){
+    result += "[";
+      _.each(obj, function(item, key) {
+        result += stringifyJSON(item);
+        if (obj.length-1 !== key) {
+          result += ",";
+        }
+      });
+    result += "]";
+
+  }
+  return result;
 };
